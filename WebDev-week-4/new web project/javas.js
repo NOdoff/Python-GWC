b3fb733dc39b6510deb2f89ad6e2f5fc2019d824
tokenadmin <tokenadmin@tokenadmins-MacBook-Pro-2.local>
2019-07-11 16:16:21 -0700

    Web Developement

---
+++ WebDev-week-4/new web project/javas.js
@@ -0,0 +1,8 @@
+function biggerPic() {
+  var pic = document.getElementById('pic')
+  pic.style.width = "250";
+}
+function smallerPic() {
+  var pic = document.getElementById('pic')
+  pic.style.width = "100";
+}
